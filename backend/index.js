dba5009285b39535dc4b67ac5f4382aee159ed8d
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-29 14:28:25 -0700

    bacend not needed

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,29 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/',(req,res) =>{
+
+    const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  res.json({response})
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
+
     res.json({
         message:'Stock API'
     })
